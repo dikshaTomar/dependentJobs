af80c3a3c8920feb2077b17f7bec815310a87152
dikshaTomar <diksha.tomar101@gmail.com>
2023-10-01 16:17:36 +0000

    running dependent jobss

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+describe('Basic Test Suite', () => {
+  it('should return "Hello, World!" when accessing the root path', () => {
+    const expectedResult = 'Hello, World!';
+    const actualResult = 'Hello, World!'; // Replace with your application's logic
+
+    assert.strictEqual(actualResult, expectedResult);
+  });
+});
+
